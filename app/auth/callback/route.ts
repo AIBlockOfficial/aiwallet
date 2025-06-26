8753401e53aaaa924af50aa276b6f798aaa0caac
Barry Botha <barry@hydracorp.ltd>
2025-06-26 08:48:39 +0300

    chore(lint): use const for response in /auth/callback to fix linter error

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -10,7 +10,7 @@ export async function GET(request: NextRequest) {
 
   if (code) {
     const cookieStore = cookies();
-    let response = NextResponse.redirect(new URL(next, request.url));
+    const response = NextResponse.redirect(new URL(next, request.url));
     const supabase = createServerClient(
       process.env.NEXT_PUBLIC_SUPABASE_URL!,
       process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
